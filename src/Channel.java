5329fd41162acb18fd42c9db9a8af288862b2bda
Kevin <toombs.kevin@gmail.com>
2016-05-03 02:30:47 -0700

    bug finally fixed. but actually.

--- src/Channel.java
+++ src/Channel.java
@@ -1,7 +1,7 @@
 public class Channel {
     private boolean DEBUG = true;
     private final java.util.concurrent.locks.Lock lock = new java.util.concurrent.locks.ReentrantLock();
-    private java.util.LinkedList<Comm> sendQueue, recvQueue;
+    private volatile java.util.LinkedList<Comm> sendQueue, recvQueue;
 
     Channel() {
         this.sendQueue = new java.util.LinkedList<>();
@@ -12,24 +12,31 @@ public class Channel {
     Object Send(Object o)
     {
         lock.lock();
+        if (DEBUG) System.out.println("Send lock.");
         //if the receive queue is empty
         if(recvQueue.isEmpty()) {
+            if (DEBUG) System.out.println("Send found empty queue.");
             //make an entry in the send queue and wait to be awoken
             java.util.concurrent.locks.Condition c = lock.newCondition();
             Comm comm = new Comm(o, c);
             sendQueue.add(comm);
             while (sendQueue.contains(comm)) {
                 try {
+                    if (DEBUG) System.out.println("Send waiting. " + c.toString());
                     c.await();
                 } catch(InterruptedException e) {
                     //pass
                 }
             }
+            if (DEBUG) System.out.println("Send done waiting.");
         } else {
             //transfer object to first recv from the queue
             recvQueue.getFirst().o = o;
+            if (DEBUG) System.out.println("Send notifying. " + recvQueue.getFirst().c.toString());
             recvQueue.getFirst().c.signal();
+            recvQueue.removeFirst();
         }
+        if (DEBUG) System.out.println("Send unlock.");
         lock.unlock();
         return o;
     }
@@ -37,29 +44,33 @@ public class Channel {
     Object Recv()
     {
         lock.lock();
-        Object o = null;
+        if (DEBUG) System.out.println("Recv lock.");
+        Object o;
         //if the send queue is empty
         if(sendQueue.isEmpty()) {
+            if (DEBUG) System.out.println("Recv found empty queue.");
             //make an entry in the recv queue and wait to be awoken
             java.util.concurrent.locks.Condition c = lock.newCondition();
             Comm comm = new Comm(null, c);
             recvQueue.add(comm);
-            while (sendQueue.contains(comm) && comm.o == null) {
+            while (recvQueue.contains(comm) && comm.o == null) {
                 try {
+                    if (DEBUG) System.out.println("Recv waiting. " + c.toString());
                     c.await();
                 } catch(InterruptedException e) {
                     //pass
                 }
             }
-            Comm out = recvQueue.removeFirst();
-            o = out.o;
-            out.c.signal();;
+            if (DEBUG) System.out.println("Recv done waiting. " + c.toString());
 
+            o = comm.o;
         } else {
-            Comm out = recvQueue.removeFirst();
+            Comm out = sendQueue.removeFirst();
             o = out.o;
-            out.c.signal();;
+            if (DEBUG) System.out.println("Recv notifying. " + out.c.toString());
+            out.c.signal();
         }
+        if (DEBUG) System.out.println("Recv unlock.");
         lock.unlock();
         return o;
     }
--- src/Demo.java
+++ src/Demo.java
@@ -1,8 +1,93 @@
 public class Demo {
 
-    public static void main(String []args) {
-        System.out.println("Hello World."); // prints Hello World
+    public void Demo1() throws InterruptedException {
         Channel hi = new Channel();
+        Thread t1 = new Thread(new SendDemo(hi));
+        Thread t3 = new Thread(new SendDemo(hi));
+        Thread t2 = new Thread(new RecvDemo(hi));
+        Thread t4 = new Thread(new RecvDemo(hi));
+
+        t1.start();
+        t3.start();
+        t2.start();
+        t4.start();
+        t1.join();
+        t3.join();
+        t2.join();
+        t4.join();
     }
 
+    public void Demo2() throws InterruptedException {
+        int size = 100;
+        Channel channel = new Channel();
+        java.util.ArrayList<Thread> threads = new java.util.ArrayList<>();
+
+        for (int i = 0; i < size; i++){
+            if(i % 2 == 0){
+                threads.add(new Thread(new SendDemo(channel)));
+            } else {
+                threads.add(new Thread(new RecvDemo(channel)));
+            }
+        }
+
+        for (int i = 0; i < size; i++) {
+            threads.get(i).start();
+        }
+
+        for (int i = 0; i < size; i++) {
+            threads.get(i).join();
+        }
+    }
+
+    public void Demo3() throws InterruptedException {
+        int size = 100;
+        Channel channel = new Channel();
+        java.util.ArrayList<Thread> threads = new java.util.ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            if (i < size / 2) {
+                threads.add(new Thread(new SendDemo(channel)));
+            } else {
+                threads.add(new Thread(new RecvDemo(channel)));
+            }
+        }
+
+        for (int i = 0; i < size; i++) {
+            threads.get(i).start();
+        }
+
+        for (int i = 0; i < size; i++) {
+            threads.get(i).join();
+        }
+    }
+
+    public class SendDemo implements Runnable {
+        Channel c;
+
+        public SendDemo(Channel in_c){
+            this.c = in_c;
+        }
+
+        public void run() {
+            String s = Thread.currentThread().toString() + " says hi!";
+            c.Send(s);
+        }
+    }
+
+    public class RecvDemo implements Runnable {
+        Channel c;
+
+        public RecvDemo(Channel in_c){
+            this.c = in_c;
+        }
+
+        public void run() {
+            Object o = c.Recv();
+            if(o instanceof String)
+            {
+                String s = (String)o;
+                System.out.println(s);
+            }
+        }
+    }
 }
